e879b3c9e097b0cbbb94806232bc67b1ab9e5eae
someshg-del <76991902+someshg-del@users.noreply.github.com>
2021-04-01 16:37:35 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+Webcam.set({
+    width: 320,
+    height: 240,
+    image_format: 'jpeg',
+    jpeg_quality: 90
+  });
+
+  camera =document.getElementById("camera");
+  
+  Webcam.attach("#camera");
+
+  function take_snapshot()
+  {
+    Webcam.snap(function(data_uri) { 
+        
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>'; });
+    
+  }
+   console.log("ml5version",ml5.version);
+  classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/v9XD3Zkek/model.json",modelLoaded);
+
+  function modelLoaded()
+  {
+     console.log("modelloaded");
+     
+  }
+
+  function check()
+  { img=document.getElementById("captured_image");
+
+    classifier.classify(img,gotResult);
+
+  }
+
+  function gotResult(error,results)
+  { if (error) {
+    console.log(error);
+    
+  }
+  else
+  { console.log(results);
+    document.getElementById("result_object_name").innerHTML=results[0].label;
+    document.getElementById("result_object_accuracy").innerHTML=results[0].confidence.toFixed(3);
+  }
+  }
